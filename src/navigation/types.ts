6bcb7a51507dabb877b49224d6fb8ac99a2641a4
arbazpirwani <arbazpirwani@gmail.com>
2025-03-06 10:45:59 +0400

    feat: define navigation types for app routes

---
+++ src/navigation/types.ts
@@ -0,0 +1,8 @@
+/**
+ * Type definitions for the navigation stack parameters
+ */
+export type RootStackParamList = {
+    Splash: undefined;
+    LanguageSelection: undefined;
+    Explore: undefined;
+};
